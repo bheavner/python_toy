2323b4ed05ef28569634697a9872e8ef60bd93f6
Ben Heavner <bheavner@gmail.com>
2017-01-18 16:34:04 -0800

    tracking hello.py

---
+++ toy/hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Hello world module"""
+
+def hello_func():
+    """print 'Hello!'"""
+    print("Hello!")
+
+def main():
+    """run hello_func"""
+    hello_func()
+
+if __name__ == '__main__':
+    main()
